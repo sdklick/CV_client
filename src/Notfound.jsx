316710fafd5f7cea883f314fc42ece3dab95e8c8
sdklick <sumantad827@gmail.com>
2024-04-06 12:15:40 +0530

    add static assets

--- src/Notfound.jsx
+++ src/Notfound.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+import nocvjpg from "../src/assets/nocv.jpg";
 
 const Notfound = () => {
   return (
     <>
       <div className="card mb-3">
         <img
-          src="src\assets\2701983yu.jpg"
+          src={nocvjpg}
           height="530px"
           className="card-img-top"
           alt="..."
